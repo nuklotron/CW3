52b8bc538a5616ab7a5af3f8aab4cfcac5eefa89
nuklotron <vas.ledkov@gmail.com>
2023-04-15 16:35:45 +0300

    update

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+from utils import load_data, last_operations, final_result
+
+
+def test_load_data():
+    assert load_data()
+    pass
+
+
+def test_last_operation():
+    assert last_operations()
+    pass
+
+
+def test_final_result():
+    assert final_result()
+    pass
+
