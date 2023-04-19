7e7a0702396cc0b9b02655f2049a20c7561003fe
nuklotron <vas.ledkov@gmail.com>
2023-04-19 17:05:00 +0300

    last update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,18 @@
-from utils import last_operations, final_result
+import json
+
+from utils import load_data, last_operations, final_result
+
+
+def test_load_data():
+    lo_da = load_data()
+    #загружаю ожидаемый результат от функции load_data() для сравнения
+    test_list = []
+    with open("tests/json_data/operations.json", encoding="utf-8") as f:
+        test_data = json.load(f)
+        for k in test_data:
+            test_list.append(k)
+
+    assert lo_da == test_list
 
 
 def test_last_operations(test_data):
@@ -10,4 +24,3 @@ def test_final_result(test_data):
     l_op = last_operations(test_data)
     f_re = final_result(l_op)
     assert f_re == ['08.12.2019 Открытие вклада\nОткрытие вклада   Счет **5907  \n41096.24 USD\n', '07.12.2019 Перевод организации\nVisa Classic 2842 87** **** 9012 -> Счет **3655  \n48150.39 USD\n', '19.11.2019 Перевод организации\nMaestro 7810 84** **** 5568 -> Счет **2869  \n30153.72 руб.\n', '13.11.2019 Перевод со счета на счет\nСчет 3861 14** **** 9794 -> Счет **8125  \n62814.53 руб.\n', '05.11.2019 Открытие вклада\nОткрытие вклада   Счет **8381  \n21344.35 руб.\n']
-
